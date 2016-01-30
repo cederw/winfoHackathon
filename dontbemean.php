8e2534505f90bdb6509a9df3150b77af2ad30a85
unknown <cederw@uw.edu>
2016-01-30 15:23:02 -0800

    added header

--- dontbemean.php
+++ dontbemean.php
@@ -70,7 +70,8 @@ try {
     // use exec() because no results are returned
     $conn->exec($sql);
 
-    echo "New record created successfully";
+    //echo "New record created successfully";
+    header('Location: index.php');
     }
 catch(PDOException $e)
     {
