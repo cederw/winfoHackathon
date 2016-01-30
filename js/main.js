8d000dd78f879eb0f7d97a29c7d6f6a9689f7792
Ivy <ivy.cm.wong@gmail.com>
2016-01-30 10:59:46 -0800

    Incoperate meaningcloud sentiment analysis api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+'use strict';
+
+var api = 'http://api.meaningcloud.com/sentiment-2.0';
+var apikey = '86c9cad7a70104cf17825ce97daa9ad6';
+var txt = '';
+var model = 'general_es';  // general_en
+
+
 $(document).ready(function() {
 	$(".emoteText").on('input', function(e) {
 		var textValue = e.target.value;
@@ -7,5 +15,23 @@ $(document).ready(function() {
 
 function determineEmote(textValue) {
 	alert(textValue);
+	$.post(api,
+    {
+        txt: textValue,
+        model: model,
+        key: apikey,
+        of: "json"
+    },
+    function(response, status){
+    	console.log(response);
+
+        alert("Status: " + status);
+        var score = response.score_tag;
+        var irony = response.irony;
+        var subjectivty = response.irony;
+        var confidence = response.confidence;
+    });
 }
+
+
 
