a614b7cbca56743c270dc1995f27f276e1620bae
Ivy <ivy.cm.wong@gmail.com>
2016-01-30 12:10:16 -0800

    Show image on popover

--- js/main.js
+++ js/main.js
@@ -5,16 +5,27 @@ var apikey = '86c9cad7a70104cf17825ce97daa9ad6';
 var txt = '';
 var model = 'general_es';  // general_en
 
+var className = ".emoteText";
+
+var image = "<img src='img/testImage.png'>";
+
 
 $(document).ready(function() {
-	$(".emoteText").on('input', function(e) {
+	$(className).on('input', function(e) {
 		var textValue = e.target.value;
+
 		determineEmote(textValue);
+        $(className).popover({
+            placement: 'right', 
+            content: image, 
+            html: true
+        });
+        $(className).popover("show");
 	});
 });
 
 function determineEmote(textValue) {
-	alert(textValue);
+	console.log(textValue);
 	$.post(api,
     {
         txt: textValue,
@@ -23,15 +34,22 @@ function determineEmote(textValue) {
         of: "json"
     },
     function(response, status){
+        console.log("Status: " + status);
     	console.log(response);
 
-        alert("Status: " + status);
         var score = response.score_tag;
         var irony = response.irony;
         var subjectivty = response.irony;
         var confidence = response.confidence;
     });
 }
+
+function checkIfSpace(text) {
+    var index = text.length;
+    var lastChar = text.substring(index - 1);
+    alert(lastChar);
+}
+
 
 
 
