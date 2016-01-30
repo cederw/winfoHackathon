c43feb01a4c5e1e830ba0534c75fa43910c26d2b
Ivy <ivy.cm.wong@gmail.com>
2016-01-30 14:14:41 -0800

    Punctuation and image switching

--- js/main.js
+++ js/main.js
@@ -3,30 +3,36 @@
 var api = 'http://api.meaningcloud.com/sentiment-2.0';
 var apikey = '86c9cad7a70104cf17825ce97daa9ad6';
 var txt = '';
-var model = 'general_es';  // general_en
+var model = 'general_en';  // general_en
 
 var className = ".emoteText";
 
-var image = "<img src='img/testImage.png'>";
-
+var imageSrc = "Neutral_None.png";
+var image = "<img src='img/" + imageSrc + "'>";
 
 $(document).ready(function() {
 	$(className).on('input', function(e) {
 		var textValue = e.target.value;
 
-		determineEmote(textValue);
-        $(className).popover({
-            placement: 'right', 
-            content: image, 
-            html: true
-        });
-        $(className).popover("show");
+        var isPunctuation = checkForPunctuation(textValue);
+
+        if(isPunctuation == true) {
+            determineEmote(textValue);
+
+            $(className).popover({
+                placement: 'right', 
+                content: image, 
+                html: true
+            });
+
+            $(className).popover("show");
+        }
+
 	});
 });
 
 function determineEmote(textValue) {
-	console.log(textValue);
-	$.post(api,
+    $.post(api,
     {
         txt: textValue,
         model: model,
@@ -34,9 +40,6 @@ function determineEmote(textValue) {
         of: "json"
     },
     function(response, status){
-        console.log("Status: " + status);
-    	console.log(response);
-
         var score = response.score_tag;
         var irony = response.irony;
         var subjectivty = response.irony;
@@ -44,12 +47,39 @@ function determineEmote(textValue) {
     });
 }
 
-function checkIfSpace(text) {
+function checkForPunctuation(text) {
+    var punctuation = [",", ".", "?", "!", ";", ":", "-", "{", "}", "(", ")", "[", "]", '"', "'", " ", "\n"];
+
     var index = text.length;
     var lastChar = text.substring(index - 1);
-    alert(lastChar);
+    
+    var result = $.inArray(lastChar, punctuation);
+
+    var isPunctuation = (result == -1) ? false : true;
+    return isPunctuation;
 }
 
+
+function determineImage(score) {
+    switch (score) {
+        case "P+":
+            imageSrc = "ExtremePositive.png";
+            break;
+        case "P":
+            imageSrc = "Positive.png";
+            break;
+        case "N":
+            imageSrc = "Negative.png";
+            break;
+        case "N+":
+            imageSrc = "ExtremeNegative.png";
+            break;
+        default:
+            imageSrc = "Neutral_None.png";
+            break;
+    }
+    return "<img src='img/" + imageSrc + "'>";
+}
 
 
 
