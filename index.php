746cf06c0c0c0c3b313a92b41693c75cba1ebd73
Shrinivas Kopparam Ramanath <shrini@uw.edu>
2016-01-30 17:38:12 -0800

    Made youtube comment section look nice

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 
-  	<title>Demo Page</title>
+  	<title>Shrini Sees</title>
   
   	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
@@ -15,12 +15,6 @@
 </head>
 
 <body>
-	<div class="container">
-		<div class="header">
-			<h1>This is a demo website</h1>
-		</div>
-	</div>
-
 	<div class="container">
 		<form action="dontbemean.php" method="get">
 
@@ -36,7 +30,7 @@
 				<input type="hidden" name="conf" id="conf">
 			</div>
 
-			<div class="form-group">
+			<div class="form-group messageBox">
 				<textarea class="emoteText" name="comm" id="comm" data-toggle="popover" data-content="" placeholder="comment"></textarea>
 			</div>
 
@@ -45,6 +39,56 @@
 			</div>
 		</form>
 	</div>	
+    
+    <div class="container" id="commentSection">
+        <?php
+        $servername = "localhost";
+        $username = "cederw_user";
+        $password = "userone";
+        $dbname = "cederw_meanless";
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql = $conn->prepare(
+                "SELECT User, Comment, Time_Posted
+                FROM COMMENT_ANALYSIS
+                ORDER BY Score DESC"
+            );
+
+            // use exec() because no results are returned
+            $sql->execute();
+
+            $result = $sql->setFetchMode(PDO::FETCH_ASSOC); 
+            foreach ($sql->fetchAll() as $k=>$v) { 
+                echo "<div class='container'>";
+                
+                echo "<div id='commentUserName'>"; 
+                echo "<a href=''>" . $v['User'] . "</a>";
+                echo "</div>";
+                                
+                echo "<div id='commentTimePosted'>"; 
+                echo $v['Time_Posted'];
+                echo "</div>";
+                              
+                echo "<div id='commentMessage'>"; 
+                echo $v['Comment'];
+                echo "</div>";
+                
+                echo "</div>";
+                
+                echo "<hr>";
+            }
+            }
+        catch(PDOException $e)
+            {
+            echo $sql . "<br>" . $e->getMessage();
+            }
+
+        $conn = null;
+        ?>        
+    </div>
 
 </body>
 </html>
